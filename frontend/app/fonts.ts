af0af0de350237be6ccf92e8cdd8b2c1f8efa6d9
Lukas Wendel <lukas.palheta@flex-am.com.br>
2024-03-12 09:50:27 -0400

    :sparkles: feat(page) new login page

---
+++ frontend/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Zen_Dots } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const zen_dots = Zen_Dots({
+    weight: "400",
+  subsets: ['latin'],
+  display: 'swap',
+})
